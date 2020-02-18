f61e2e71cfd10fab4d3bf9136353508e3d172e9a
Lee <lee.a.howard@hotmail.co.uk>
2020-02-18 14:59:48 +0000

    Pull data from endpoint

--- loader.py
+++ loader.py
@@ -1,9 +1,9 @@
 # Import the json module
 import json
+import urllib.request
 from orator import DatabaseManager, Schema
 
-from pprint import pprint
-
+api_endpoint = "https://api.nanoporetech.com/oracle/v1/requisitions"
 
 data_fields = {"city": {"type": "string"},
         "country": {"type": "string"},
@@ -29,23 +29,20 @@ db_config = {
     }
 }
 
+# Create DB Manager and Schema for interacting with the MySQL
 db = DatabaseManager(db_config)
 schema = Schema(db)
 
-# TODO: Spin up docker container here
-# sudo docker run -d -p 3306:3306 -e MYSQL_DATABASE=apitest -e MYSQL_USER=test-user -e MYSQL_PASSWORD=test-pass mysql/mysql-server --default_authentication_plugin=mysql_native_password
+# Retrieve data from the API endpoint
+print(f"Retrieving data from {api_endpoint}")
+with urllib.request.urlopen(api_endpoint) as url:
+    json_data = json.loads(url.read().decode())
 
-
-# TODO: Replace reading file with pulling from API endpoint
-
-# Load the file 
-json_data = json.loads(open('data.json').read())
- 
-# Set field sizes
+# Set field sizes - we know some are longer than 255
+print(f"Setting field sizes")
 for field in data_fields.keys():
 
     # Max of length of values of entries in the array
-    print(field)
     seq = [len(x[field]) for x in json_data if x[field]]
 
     if max(seq) > 255:
@@ -53,12 +50,9 @@ for field in data_fields.keys():
     else:
         data_fields[field]["size"] = 255
 
-    
-    print(f"  Max length: {data_fields[field]['size']}")
-
-
 
 # Create a table
+print("Creating DB table named 'data'")
 with schema.create('data') as table:
     for field in data_fields.keys():
         if data_fields[field]["type"] == "string": table.string(field, data_fields[field]["size"]).nullable()
@@ -67,5 +61,8 @@ with schema.create('data') as table:
 
 
 # Fire in the data
+print("Writing records")
 for record in json_data:
     db.table('data').insert(record)
+
+print("Complete!")
